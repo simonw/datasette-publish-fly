fb09455ad0634596de946cf65aed60458f513d9d
Simon Willison <swillison@gmail.com>
2022-02-10 16:57:55 -0800

    Integration test for deploy with volumes, refs #10

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -44,6 +44,58 @@ def test_basic():
     assert app["Deployed"] is True
 
 
+def test_with_volume():
+    runner = CliRunner()
+    app_name = APP_PREFIX + "v-" + secrets.token_hex(4)
+    with runner.isolated_filesystem():
+        sqlite3.connect("test.db").execute("create table foo (id integer primary key)")
+        result = runner.invoke(
+            cli.cli,
+            [
+                "publish",
+                "fly",
+                "test.db",
+                "-a",
+                app_name,
+                "--create-volume",
+                "1",
+                "--create-db",
+                "writeme",
+                "--install",
+                "datasette-graphql",
+                "--plugin-secret",
+                "foo",
+                "bar",
+                "baz",
+                "--show-files",
+            ],
+            catch_exceptions=False,
+        )
+        assert result.exit_code == 0, result.output
+    # These fragments are expected in Dockerfile or fly.toml or metadata.json
+    fragments = (
+        "CMD datasette serve --host 0.0.0.0 -i test.db",
+        "/data/writeme.db --create --port $PORT /data/*.db",
+        'destination = "/data"\n  source = "datasette"' '"$env": "FOO_BAR"',
+    )
+    for fragment in fragments:
+        assert fragment in result.output
+    # Confirm app has deployed
+    apps = get_apps()
+    matches = [a for a in apps if a["Name"] == app_name]
+    assert matches, "No app found with expected name: " + app_name
+    app = matches[0]
+    assert app["Status"] == "running"
+    assert app["Deployed"] is True
+    # That app should also have a volume
+    volumes = get_volumes(app_name)
+    assert len(volumes) == 1
+    assert volumes[0]["Name"] == "datasette"
+    # Check secrets - there's no --json for that yet
+    app_secrets = get_secrets(app_name)
+    assert "FOO_BAR" in app_secrets
+
+
 def cleanup_any_resources():
     app_names = [app["Name"] for app in get_apps()]
     # Delete any starting with publish-fly-temp-
@@ -59,3 +111,22 @@ def get_apps():
         stderr=subprocess.PIPE,
     )
     return json.loads(process.stdout)
+
+
+def get_volumes(app_name):
+    process = subprocess.run(
+        ["flyctl", "volumes", "list", "-a", app_name, "--json"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    return json.loads(process.stdout)
+
+
+def get_secrets(app_name):
+    # No --json for this yet, so just returns a big ugly string
+    process = subprocess.run(
+        ["flyctl", "secrets", "list", "-a", app_name],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    return process.stdout.decode("utf-8")
